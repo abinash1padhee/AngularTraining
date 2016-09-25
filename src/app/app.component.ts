b393552188682e400ff1543d502df1a9fcd3b02c
Abinash Padhee <abinash1padhee@gmail.com>
2016-09-25 07:18:33 +0530

    Added code for Services and Promises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
-import {Component, Input} from '@angular/core';
-import {Hero} from './hero';
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from "./hero";
+import { HeroService } from './hero.service';
 
 
-const HEROES: Hero[] = [
-    {id: 11, name: 'Mr. Nice'},
-    {id: 12, name: 'Narco'},
-    {id: 13, name: 'Bombasto'},
-    {id: 14, name: 'Celeritas'},
-    {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
-];
 
 
 @Component({
     selector: 'my-app',
+    providers: [HeroService],
     template: `
       <h1>{{title}}</h1>
       <h2>My Heroes</h2>
@@ -85,10 +75,24 @@ const HEROES: Hero[] = [
 })
 
 
-export class AppComponent {
-    heroes = HEROES;
+export class AppComponent implements OnInit {
+    heroes: Hero[];
     title = 'Tour of Heroes';
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+
+
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
